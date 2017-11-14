ea1b20c3509975dcc12a9c53bc7169250c9c6731
Joe Archer <joe@laserred.co>
2017-11-14 23:06:44 +0000

    yes no for is_in

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,3 @@
 @foreach($devices as $device)
-    <p>{{$device->name}} In? {{$device->is_in}} {{$device->updated_at->format('d/m/Y H:i:s')}}</p>
+    <p>{{$device->name}} In? {{$device->is_in ? 'Yes' : 'No'}} {{$device->updated_at->format('d/m/Y H:i:s')}}</p>
 @endforeach
