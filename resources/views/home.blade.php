5e60f0d8684de7e8264ecf632230cdc16910cf30
Joe Archer <joe@laserred.co>
2017-11-14 22:36:44 +0000

    Carbon date

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,3 @@
 @foreach($devices as $device)
-    <p>{{$device->name}} In? {{$device->is_in}} {{date('d/m/Y H:i:s', $device->updated_at)}}</p>
+    <p>{{$device->name}} In? {{$device->is_in}} {{$device->updated_at->format('d/m/Y H:i:s')}}</p>
 @endforeach
