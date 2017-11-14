d4082a0d6a65c7adc77020ae5567c2d0ec1d748d
Joe Archer <joe@laserred.co>
2017-11-14 22:22:03 +0000

    var dump devices

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\ScanService;
 
+use App\Device;
+
 class HomeController extends Controller
 {
     public function index(){
-        return ScanService::doScan();
+        return var_dump(Device::all());
     }
 }
